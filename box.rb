0fef6542d74bb660cf04edb7394424e5cb92bc3b
grime3ja <grime3ja@dukes.jmu.edu>
2025-09-02 15:05:17 -0400

    evaluator done?

--- box.rb
+++ box.rb
@@ -4,33 +4,23 @@ require_relative "translator"
 
 # sum = Add.new(IntegerPrimitive.new(5), IntegerPrimitive.new(4))
 #text = sum.visit(Translator.new())
-<<<<<<< HEAD
 # neg = Negation.new(IntegerPrimitive.new(5))
-# text = neg.visit(Translator.new())
-# sub = Subtraction.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-# text = sub.visit(Translator.new())
-# mult = Multiply.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-# text = mult.visit(Translator.new())
-# div = Divide.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-# text = div.visit(Translator.new())
-# mod = Modulo.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-# text = mod.visit(Translator.new())
-exp = Exponent.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-text = exp.visit(Translator.new())
-=======
-#neg = Negation.new(IntegerPrimitive.new(5))
 #text = neg.visit(Translator.new())
-#sub = Subtraction.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
+# sub = Subtraction.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
 #text = sub.visit(Translator.new())
-#mult = Multiply.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
+# mult = Multiply.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
 #text = mult.visit(Translator.new())
-#div = Divide.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
+# div = Divide.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
 #text = div.visit(Translator.new())
-#mod = Modulo.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
+# mod = Modulo.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
 #text = mod.visit(Translator.new())
-#exp = Exponent.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
+# exp = Exponent.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
 #text = exp.visit(Translator.new())
-arth = Modulo.new(Add.new(Multiply.new(IntegerPrimitive.new(7),IntegerPrimitive.new(3)),IntegerPrimitive.new(8)),IntegerPrimitive.new(12))
-text = arth.visit(Translator.new())
->>>>>>> faca94338d680029f6b91c3a89847bbf7bc9ee45
+# arth = Modulo.new(Add.new(Multiply.new(IntegerPrimitive.new(7),IntegerPrimitive.new(3)),IntegerPrimitive.new(8)),IntegerPrimitive.new(12))
+# text = arth.visit(Translator.new())
+# p text
+
+arth = Add.new(Add.new(Add.new(IntegerPrimitive.new(5), IntegerPrimitive.new(3)), IntegerPrimitive.new(2)), IntegerPrimitive.new(1))
+text = arth.visit(Translator.new) + " =  #{arth.visit(Evaluator.new)}"
+
 p text
--- evaluator.rb
+++ evaluator.rb
@@ -1,8 +1,70 @@
 class Evaluator
-    def visit_add(node)
-        leftPrimitive = node.left.evaluate
-        rightPrimitive = node.right.evaluate
+    def visit_integer(node)
+        node.value
+    end
 
-        leftPrimitive.value + rightPrimitive.value
+    def visit_float(node)
+        node.value
+    end
+
+    def visit_boolean(node)
+        node.value
+    end
+
+    def visit_string(node)
+        node.value
+    end
+
+    def visit_null(node)
+        node.value
+    end
+
+    def visit_add(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive + rightPrimitive
+    end
+
+    def visit_subtract(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive - rightPrimitive
+    end
+
+    def visit_multiply(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive * rightPrimitive
+    end
+
+    def visit_divide(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive / rightPrimitive
+    end
+
+    def visit_modulo(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive % rightPrimitive
+    end
+
+    def visit_exponent(node)
+        leftPrimitive = node.left.visit(self)
+        rightPrimitive = node.right.visit(self)
+
+        leftPrimitive ** rightPrimitive
+    end
+
+    # still need to do this part
+    def visit_negate(node)
+        value = node.value
+
+        -value
     end
 end
