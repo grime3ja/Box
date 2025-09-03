43957ae3a21f6a3aafee4820a28526328af0122a
grime3ja <grime3ja@dukes.jmu.edu>
2025-09-03 11:32:50 -0400

    testing questions 1-2

--- box.rb
+++ box.rb
@@ -7,57 +7,20 @@ require_relative "cast"
 require_relative "variable"
 require_relative "relational"
 
-# sum = Add.new(IntegerPrimitive.new(5), IntegerPrimitive.new(4))
-#text = sum.visit(Translator.new())
-# neg = Negation.new((Negation.new(IntegerPrimitive.new(5))))
-# text = neg.visit(Translator.new())
-# sub = Subtraction.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-#text = sub.visit(Translator.new())
-# mult = Multiply.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-#text = mult.visit(Translator.new())
-# div = Divide.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-#text = div.visit(Translator.new())
-# mod = Modulo.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-#text = mod.visit(Translator.new())
-# exp = Exponent.new(IntegerPrimitive.new(7), IntegerPrimitive.new(2))
-#text = exp.visit(Translator.new())
-# arth = Modulo.new(Add.new(Multiply.new(IntegerPrimitive.new(7),IntegerPrimitive.new(3)),IntegerPrimitive.new(8)),IntegerPrimitive.new(12))
-# text = arth.visit(Translator.new())
-# p text
+### Video Examples ###
 
-# arth = Add.new(Add.new(Add.new(IntegerPrimitive.new(5), IntegerPrimitive.new(3)), IntegerPrimitive.new(2)), IntegerPrimitive.new(1))
-# text = arth.visit(Translator.new) + " =  #{arth.visit(Evaluator.new)}"
+arithmetic = Modulo.new(
+               Add.new(
+                 Multiply.new(IntegerPrimitive.new(7), IntegerPrimitive.new(4)),
+                 IntegerPrimitive.new(3)
+               ), IntegerPrimitive.new(12)
+             )
 
-# p text
+text = "#{arithmetic.visit(Translator.new)} = #{arithmetic.visit(Evaluator.new)}"
+p text
 
-# and_test = And.new(BooleanPrimitive.new(true), BooleanPrimitive.new(false))
-# text = and_test.visit(Evaluator.new())
-# or_test = Or.new(BooleanPrimitive.new(true), BooleanPrimitive.new(false))
-# text = or_test.visit(Evaluator.new())
-# not_test = Not.new(BooleanPrimitive.new(true))
-# text = not_test.visit(Evaluator.new())
-# bit_x = BitRight.new(IntegerPrimitive.new(9), IntegerPrimitive.new(2))
-# text = bit_x.visit(Evaluator.new())
-
-# eq = Equals.new(IntegerPrimitive.new(4), IntegerPrimitive.new(3))
-# text = eq.visit(Translator.new())
-# p text
-# text = eq.visit(Evaluator.new())
-# p text
-# int_test = IntToFloat.new(BooleanPrimitive.new(5))
-# text = int_test.visit(Translator.new())
-# p text
-
-var_x = VarPrimitive.new("x")
-assign_test = Assignment.new(var_x, IntegerPrimitive.new(5))
-r_test = VarReference.new(assign_test)
-# text = r_test.visit(Translator.new())
-
-var_y = VarPrimitive.new('y')
-assign_y = Assignment.new(var_y, IntegerPrimitive.new(3))
-y_test = VarReference.new(assign_y)
-sum = Multiply.new(r_test, y_test)
-
-text = sum.visit(Evaluator.new)
-
+a = Assignment.new(VarPrimitive.new('a'), IntegerPrimitive.new(-4))
+b = Assignment.new(VarPrimitive.new('b'), IntegerPrimitive.new(-3))
+arithmetic_negation_and_rvals = Multiply.new(VarReference.new(a), VarReference.new(b))
+text = "#{arithmetic_negation_and_rvals.visit(Translator.new)} = #{arithmetic_negation_and_rvals.visit(Evaluator.new)}"
 p text
