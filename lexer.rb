7011638ee6fce941068e91433e995b7344e48a98
grime3ja <grime3ja@dukes.jmu.edu>
2025-09-23 19:07:46 -0400

    more token types

--- lexer.rb
+++ lexer.rb
@@ -42,10 +42,69 @@ class Lexer
             while has_digit
                 capture
             end
+            if has(".")
+                capture
+                if has_digit
+                    while has_digit
+                        capture
+                    end
+                    emit_token(:float_literal)
+                else
+                    raise "Expected one or more digits at index #{@index} following the decimal point"
+                end
+            else
                 emit_token(:integer_literal)
+            end
         elsif has("+")
             capture
             emit_token(:addition)
+        elsif has("-")
+            capture
+            emit_token(:subtraction)
+        elsif has("*")
+            capture
+            emit_token(:multiplication)
+        elsif has("/")
+            capture
+            emit_token(:division)
+        elsif has("**")
+            capture
+            capture
+            emit_token(:exponentiation)
+        elsif has("%")
+            capture
+            emit_token(:modulo)
+        elsif has("&&")
+            capture
+            capture
+            emit_token(:and)
+        elsif has("||")
+            capture
+            capture
+            emit_token(:or)
+        elsif has("!")
+            capture
+            emit_token(:not)
+        elsif has("&")
+            capture
+            emit_token(:bit_and)
+        elsif has("|")
+            capture
+            emit_token(:bit_or)
+        elsif has("^")
+            capture
+            emit_token(:bit_xor)
+        elsif has("~")
+            capture
+            emit_token(:bit_not)
+        elsif has("<<")
+            capture
+            capture
+            emit_token(:bit_left)
+        elsif has(">>")
+            capture
+            capture
+            emit_token(:bit_right)
         else
             while has(" ")
                 @index += 1
@@ -61,7 +120,7 @@ class Lexer
     end
 end
 
-lex = Lexer.new("5 + 23")
+lex = Lexer.new("7 * 4.6 + 3 % 12")
 tokens = lex.lex_string
 for token in tokens
     p token  
